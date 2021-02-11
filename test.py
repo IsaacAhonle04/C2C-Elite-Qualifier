10146c1207fff2ecfa0e9cdd7d53f6065ce6d35f
Isaac Ahonle <isaacahonle04@gmail.com>
2021-02-11 22:22:34 +0000

    Unit Testing Update (Manual)

---
+++ test.py
@@ -0,0 +1,17 @@
+import unittest
+from main import generate_response_1
+#from main import generate_response_2
+#from main import generate_response_3
+#from main import generate_response_4
+#from main import generate_response_good
+#from main import generate_response_bad
+class TestResponseGeneration(unittest.TestCase):
+  def test_set1_1(self):
+    self.assertEqual (generate_response_1(user_input="Hello!"),"Interesting!")
+  def test_set1_2(self):
+    self.assertEqual (generate_response_1(user_input="Hello!"),"Nice one!")    
+  def test_set1_3(self):
+    self.assertEqual (generate_response_1(user_input="Hello!"),"Me too!")
+
+if __name__ == '__main__':
+    unittest.main()
