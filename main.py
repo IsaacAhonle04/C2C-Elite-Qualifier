4e4dba1e515e5168ee84394200bb73c7d882269c
Isaac Ahonle <isaacahonle04@gmail.com>
2021-02-23 06:10:24 +0000

    Set up the Personality selection framework

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import random
 
+#Base personality response sets
 def generate_response_1b(user_input):
   response_set_1b = [
     "Interesting!",
-   # "Nice one!",
-   # "Me too!"
+    "Nice one!",
+    "Me too!"
   ]
   return random.choice(response_set_1b)
 
@@ -49,31 +50,139 @@ def generate_response_good(user_input):
   ]
   return random.choice(doing_good)
 
+#Cold personality response sets
+def generate_response_1c(user_input):
+  response_set_1c = [
+    "",
+    "",
+    ""
+  ]
+  return random.choice(response_set_1c)
+
+def generate_response_2c(user_input):
+  response_set_2c = [
+    "",
+    "",
+    ""
+  ]
+  return random.choice(response_set_2c)
+
+def generate_response_3c(user_input):
+  response_set_3c = [
+    "",
+    "",
+    ""
+  ]
+  return random.choice(response_set_3c)
+
+def generate_response_4c(user_input):
+  response_set_4c = [
+    "",
+    "",
+    ""
+  ]
+  return random.choice(response_set_4c)
+
+#Snarky personality response sets
+def generate_response_1s(user_input):
+  response_set_1s = [
+    "",
+    "",
+    ""
+  ]
+  return random.choice(response_set_1s)
+
+def generate_response_2s(user_input):
+  response_set_2s = [
+    "",
+    "",
+    ""
+  ]
+  return random.choice(response_set_2s)
+
+def generate_response_3s(user_input):
+  response_set_3s = [
+    "",
+    "",
+    ""
+  ]
+  return random.choice(response_set_3s)
+
+def generate_response_4s(user_input):
+  response_set_4s = [
+    "",
+    "",
+    ""
+  ]
+  return random.choice(response_set_4s)
+
+# Main code  
+
 def init_chat():
   quit_character = 'q'
 
-  user_input = input("Hello, how are you?\n")
+  user_input = input("Thank you for using ChatBot 2.0! Which personality would you like? Your options are Basic, Cold, and Snarky.\n")
+  while 1==1:
+    if "Basic" in user_input:
+      user_input = input("Hello, how are you?\n")
 
-  while user_input != quit_character:
-    if "good" in user_input:
-        user_input = input(generate_response_good(user_input) + "\n")      
-    elif "bad" in user_input:
-        user_input = input(generate_response_bad(user_input) + "\n")
-    elif "!" in user_input:
-      if "." in user_input:
-        user_input = input(generate_response_4b(user_input) + "\n")
-      else:
-        user_input = input(generate_response_1b(user_input) + "\n")
-    elif "." in user_input:
-      if "!" in user_input:
-        user_input = input(generate_response_4b(user_input) + "\n")
-      else:
-        user_input = input(generate_response_2b(user_input) + "\n")
-    elif "?" in user_input:
-        user_input = input(generate_response_3b(user_input) + "\n")   
+      while user_input != quit_character:
+        if "good" in user_input:
+            user_input = input(generate_response_good(user_input) + "\n")      
+        elif "bad" in user_input:
+            user_input = input(generate_response_bad(user_input) + "\n")
+        elif "!" in user_input:
+          if "." in user_input:
+            user_input = input(generate_response_4b(user_input) + "\n")
+          else:
+            user_input = input(generate_response_1b(user_input) + "\n")
+        elif "." in user_input:
+          if "!" in user_input:
+            user_input = input(generate_response_4b(user_input) + "\n")
+          else:
+            user_input = input(generate_response_2b(user_input) + "\n")
+        elif "?" in user_input:
+            user_input = input(generate_response_3b(user_input) + "\n")   
+        else:
+            user_input = input(generate_response_4b(user_input) + "\n")        
+    elif "Cold" in user_input:
+      user_input = input("Hello, how are you?\n")
+
+      while user_input != quit_character:
+        if "!" in user_input:
+          if "." in user_input:
+            user_input = input(generate_response_4c(user_input) + "\n")
+          else:
+            user_input = input(generate_response_1c(user_input) + "\n")
+        elif "." in user_input:
+          if "!" in user_input:
+            user_input = input(generate_response_4c(user_input) + "\n")
+          else:
+            user_input = input(generate_response_2c(user_input) + "\n")
+        elif "?" in user_input:
+            user_input = input(generate_response_3c(user_input) + "\n")   
+        else:
+            user_input = input(generate_response_4c(user_input) + "\n")          
+    elif "Snarky" in user_input:
+      user_input = input("Hello, how are you?\n")
+
+      while user_input != quit_character:
+        if "!" in user_input:
+          if "." in user_input:
+            user_input = input(generate_response_4s(user_input) + "\n")
+          else:
+            user_input = input(generate_response_1s(user_input) + "\n")
+        elif "." in user_input:
+          if "!" in user_input:
+            user_input = input(generate_response_4s(user_input) + "\n")
+          else:
+            user_input = input(generate_response_2s(user_input) + "\n")
+        elif "?" in user_input:
+            user_input = input(generate_response_3s(user_input) + "\n")   
+        else:
+            user_input = input(generate_response_4s(user_input) + "\n")        
     else:
-        user_input = input(generate_response_4b(user_input) + "\n")         
-
-
+      user_input = input("Please enter an available option.")
+    
 if __name__ == "__main__":
   init_chat()
